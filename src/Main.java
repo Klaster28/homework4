79789569b09db917c2dc3bc3166e4067be747c30
Klaster28 <89096914521@mail.ru>
2022-09-27 20:29:59 +0300

    Коммит после первого задания

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age>=18){
+            System.out.println("Поздравляю Вас с совершеннолетием!!!");
+        }
+        if (age < 18) {
+            System.out.println("Вам необходимо дождаться совершеннолетия (((");
+        }
+
+    }
+}
